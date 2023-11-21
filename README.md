809c6f6c7cac701b9159381f3d8b5fdd097575ed
Ignacio <projetosignacio@gmail.com>
2023-11-20 23:49:01 -0300

    "Alterações finais"

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Reproduzindo pagina
-Projeto de Página
+# Reproduzindo pagina:
+Projeto aonde um grupo de 3 pessoas desenvolveram um template:
+
+https://www.tooplate.com/view/2086-multi-color
+
+# Colaboradores:
+
+https://github.com/EloiseHelena
+
+https://github.com/AlicksonFreitas
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -17,3 +17,102 @@ containImgs.forEach(containImg => {
   });
 });
 
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var buttonColors = {
+//     'box1': '#38b6e6',
+//     'box2': '#ed544c',
+//     'box3': '#eaa319',
+//     'box4': '#16b5a3',
+//     'box5': '#3f97c9'
+//   };
+
+//   var bodyColors = [
+//     '#38b4e69e',
+//     '#ed544c9e',
+//     '#eaa3199e',
+//     '#16b5a39e',
+//     '#457ec99e'
+//   ];
+
+//   var buttons = document.querySelectorAll('button');
+//   var body = document.querySelector('body');
+
+//   setInitialState();
+
+//   buttons.forEach(function(button, index) {
+//     button.addEventListener('click', function() {
+//       buttons.forEach(function(btn) {
+//         btn.classList.remove('select');
+//         btn.style.backgroundColor = 'transparent';
+//         btn.style.color = '#000';
+//       });
+
+//       this.classList.add('select');
+//       this.style.backgroundColor = buttonColors[this.classList[0]];
+
+//       body.style.backgroundColor = bodyColors[index];
+//     });
+//   });
+
+
+//   function setInitialState() {
+//     buttons[0].classList.add('select');
+//     buttons[0].style.backgroundColor = buttonColors['box1'];
+//     body.style.backgroundColor = bodyColors[0];
+//   }
+// });
+document.addEventListener('DOMContentLoaded', function() {
+  var buttonColors = {
+    'box1': '#38b6e6',
+    'box2': '#ed544c',
+    'box3': '#eaa319',
+    'box4': '#16b5a3',
+    'box5': '#3f97c9'
+  };
+
+  var bodyColors = [
+    '#38b4e69e',
+    '#ed544c9e',
+    '#eaa3199e',
+    '#16b5a39e',
+    '#457ec99e'
+  ];
+
+  var buttons = document.querySelectorAll('.buttonHeader');
+  var body = document.querySelector('body');
+  var sections = document.querySelectorAll('section');
+
+  setInitialState();
+
+  buttons.forEach(function(button, index) {
+    button.addEventListener('click', function() {
+      buttons.forEach(function(btn) {
+        btn.classList.remove('select');
+        btn.style.backgroundColor = 'transparent';
+        btn.style.color = '#000';
+      });
+
+      this.classList.add('select');
+      this.style.backgroundColor = buttonColors[this.classList[0]];
+
+      body.style.backgroundColor = bodyColors[index];
+      sections.forEach(function(section, sectionIndex) {
+        if (index === sectionIndex) {
+          section.classList.remove('hide-off');
+        } else {
+          section.classList.add('hide-off');
+        }
+      });
+    });
+  });
+
+  function setInitialState() {
+
+    buttons[0].classList.add('select');
+    buttons[0].style.backgroundColor = buttonColors['box1'];
+    body.style.backgroundColor = bodyColors[0];
+
+    sections[0].classList.remove('hide-off');
+  }
+});
